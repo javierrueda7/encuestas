03cd770418c92cada47fd7352abd8d112a6e3527
javieruedase <javieruedase@gmail.com>
2024-05-27 16:40:53 -0500

    done

---
+++ lib/initpage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:forms_app/mainmenu.dart';
+import 'package:forms_app/newuser.dart';
+
+class InitPage extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _InitPageState createState() => _InitPageState();
+}
+
+class _InitPageState extends State<InitPage> {
+  
+  void _reloadList() {
+    setState(() {}); // Empty setState just to trigger rebuild
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('INICIO')),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/LogoCyMA.png',
+                height: 250,
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainMenu()), // Navigate to the MainMenu
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: TextStyle(fontSize: 18),
+                ),
+                child: Text('MENÚ PRINCIPAL'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AddEditUser(reloadList: _reloadList, admin: false,);
+                    }
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: TextStyle(fontSize: 18),
+                ),
+                child: Text('CREAR USUARIO'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,30 @@
-import 'package:flutter/material.dart';
-import 'package:forms_app/mainmenu.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:forms_app/newuser.dart';
-import 'firebase_options.dart';
-import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:forms_app/firebase_options.dart';
+import 'package:forms_app/initpage.dart';
 
-Future<void> main() async {
+// Pages
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    print("Firebase initialized successfully");
-  } catch (e) {
-    print("Error initializing Firebase: $e");
-  }
-  runApp(EncuestasMOP());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
-  
 
-class EncuestasMOP extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CYMA - ENCUESTAS MOP',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MainPage(), // Use the MainPage as the home page
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  @override
-  // ignore: library_private_types_in_public_api
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  
-  void _reloadList() {
-    setState(() {}); // Empty setState just to trigger rebuild
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('INICIO')),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'images/LogoCyMA.png',
-                height: 250,
-              ),
-              SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MainMenu()), // Navigate to the MainMenu
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  textStyle: TextStyle(fontSize: 18),
-                ),
-                child: Text('MENÚ PRINCIPAL'),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AddEditUser(reloadList: _reloadList, admin: false,);
-                    }
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  textStyle: TextStyle(fontSize: 18),
-                ),
-                child: Text('CREAR USUARIO'),
-              ),
-            ],
-          ),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => InitPage(),
+      },
     );
   }
 }
